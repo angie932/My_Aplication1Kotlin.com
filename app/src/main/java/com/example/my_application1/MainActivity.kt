d631f4c44685f8ab48450233d07dbff85bd96c52
Ex1Kx <thenewestekigoh@gmail.com>
2022-04-07 15:49:07 -0500

    Se agrega la carpeta de android

---
+++ app/src/main/java/com/example/my_application1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.my_application1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
